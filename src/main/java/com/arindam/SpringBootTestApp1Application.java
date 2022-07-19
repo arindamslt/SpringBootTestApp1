ab156d08a687241a6eb0e13fe397c1a747e20e95
arindamslt <arin.saltlake@gmail.com>
2022-07-19 21:14:13 +0530

    SECOND COMMIT

---
+++ src/main/java/com/arindam/SpringBootTestApp1Application.java
@@ -0,0 +1,13 @@
+package com.arindam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestApp1Application.class, args);
+	}
+
+}
